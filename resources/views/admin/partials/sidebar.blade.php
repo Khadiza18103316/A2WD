e9b3ea12af3dc9d857716c8346fef000df49af07
Khadiza <khadizaakhi756@gmail.com>
2022-04-01 00:44:20 +0600

    tamplate added

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,94 @@
+<div id="sidebar" class="active">
+    <div class="sidebar-wrapper active">
+       
+        <div class="sidebar-menu">
+            <ul class="menu">
+
+                <li class="sidebar-item active ">
+                    <a href="index.html" class='sidebar-link'>
+                        <i class="bi bi-grid-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+
+               
+
+                <li class="sidebar-item  ">
+                    <a href="table.html" class='sidebar-link'>
+                        <i class="bi bi-grid-1x2-fill"></i>
+                        <span>Table</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="table-datatable.html" class='sidebar-link'>
+                        <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                        <span>Datatable</span>
+                    </a>
+                </li>
+
+               
+
+                <li class="sidebar-item  ">
+                    <a href="ui-file-uploader.html" class='sidebar-link'>
+                        <i class="bi bi-cloud-arrow-up-fill"></i>
+                        <span>File Uploader</span>
+                    </a>
+                </li>
+
+              
+
+                <li class="sidebar-item  ">
+                    <a href="application-email.html" class='sidebar-link'>
+                        <i class="bi bi-envelope-fill"></i>
+                        <span>Email Application</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="application-chat.html" class='sidebar-link'>
+                        <i class="bi bi-chat-dots-fill"></i>
+                        <span>Chat Application</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="application-gallery.html" class='sidebar-link'>
+                        <i class="bi bi-image-fill"></i>
+                        <span>Photo Gallery</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="application-checkout.html" class='sidebar-link'>
+                        <i class="bi bi-basket-fill"></i>
+                        <span>Checkout Page</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="https://zuramai.github.io/mazer/docs" class='sidebar-link'>
+                        <i class="bi bi-life-preserver"></i>
+                        <span>Documentation</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="https://github.com/zuramai/mazer/blob/main/CONTRIBUTING.md" class='sidebar-link'>
+                        <i class="bi bi-puzzle"></i>
+                        <span>Contribute</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="https://github.com/zuramai/mazer#donate" class='sidebar-link'>
+                        <i class="bi bi-cash"></i>
+                        <span>Donate</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,355 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Digital  |  Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css" />
+	<link rel="stylesheet" href="{{url('frontend/./assets/css/main.css')}}">
+	<link rel="stylesheet" href="{{url('frontend/./assets/css/style.css')}}">
+	<link rel="stylesheet" href="{{url('frontend/./assets/css/slider.css')}}">
+	<link rel="stylesheet" href="{{url('frontend/./assets/css/gallery.css')}}">
+
+	</head>
+	<body>
+
+		<!-- navbar start here -->
+			<nav class="fixed-top ftco-navbar-light ftco_navbar navbar navbar-expand-lg">
+		    <div class="container">
+		    	<a class="navbar-brand" href="index.html">
+		    		<img src="{{url('frontend/./assets/img/logo.png')}}" alt="" class="logo" />
+		    	 Digital
+		    	</a>
+		    	<div class="social-media order-lg-last">
+		    		<p class="mb-0 d-flex">
+		    			<a href="#" class="d-flex align-items-center justify-content-center"><i class="fa-brands fa-facebook-f"></i></a>
+		    			<a href="#" class="d-flex align-items-center justify-content-center"><i class="fa-brands fa-twitter"></i></a>
+		    			<a href="#" class="d-flex align-items-center justify-content-center"><i class="fa-brands fa-instagram"></i></a>
+		    		</p>
+	        </div>
+		      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+		        <span class="fa fa-bars"></span>
+		      </button>
+		      <div class="collapse navbar-collapse" id="ftco-nav">
+		        <ul class="navbar-nav ml-auto mr-md-3 text-center">
+		        	<li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
+		        	<li class="nav-item"><a href="./pages/about.html" class="nav-link">About</a></li>
+		        	<li class="nav-item"><a href="./pages/gallery.html" class="nav-link">Gallery</a></li>
+		        	<li class="nav-item"><a href="./pages/teammates.html" class="nav-link">Team mates</a></li>
+		          <li class="nav-item"><a href="#contact" class="nav-link">Contact</a></li>
+		        </ul>
+		      </div>
+		    </div>
+		  </nav>
+    <!-- END nav -->
+
+
+
+<div class="slider" id="slider1">
+    <!-- Slides -->
+    <div style="background-image:url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/30256/1200_110627-8240-Myst.jpg)"></div>
+    <div style="background-image:url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/30256/Gif-pont-lueur-600.gif)"></div>
+    <div style="background-image:url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/30256/1200_Sharpened-version.jpg)"></div>
+    <div style="background-image:url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/30256/jungle.jpg)"></div>
+    <div style="background-image:url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/30256/1200_bodie-11.jpg)"></div>
+    <!-- The Arrows -->
+    <i class="left" class="arrows" style="z-index:2; position:absolute;"><svg viewBox="0 0 100 100">
+            <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z"></path>
+        </svg></i>
+    <i class="right" class="arrows" style="z-index:2; position:absolute;"><svg viewBox="0 0 100 100">
+            <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" transform="translate(100, 100) rotate(180) "></path>
+        </svg></i>
+    <!-- Title Bar -->
+    <span class="titleBar">
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+    </span>
+</div>
+
+
+
+
+
+
+<!-- gallery start here -->
+<div class="container py-5">
+	<div class="row">
+		<div class="col-lg-12 text-center my-2">
+			<h3 class="m-0">Gallery</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+		</div>
+	</div>
+	<div class="portfolio-item row pt-2">
+		<div class="item selfie col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-psd/front-view-photographer-wooden-workspace-with-computer-monitor_23-2149021348.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-psd/front-view-photographer-wooden-workspace-with-computer-monitor_23-2149021348.jpg" alt="">
+			</a>
+		</div>
+		<div class="item gts col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-photo/laptop-pens-phone-note-with-blank-screen-table_155003-5339.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-photo/laptop-pens-phone-note-with-blank-screen-table_155003-5339.jpg" alt="">
+			</a>
+		</div>
+		<div class="item  col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-psd/modern-computer-screen-mockup-v3_317037-127.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-psd/modern-computer-screen-mockup-v3_317037-127.jpg" alt="">
+			</a>
+		</div>
+		<div class="item gts col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-vector/work-from-home-illustration-with-copy-space-man-working-with-laptop-desktop-flat-design_115968-28.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-vector/work-from-home-illustration-with-copy-space-man-working-with-laptop-desktop-flat-design_115968-28.jpg" alt="">
+			</a>
+		</div>
+		<div class="item gts col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-psd/computer-screen-blank-background-mock-up_165789-570.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-psd/computer-screen-blank-background-mock-up_165789-570.jpg" alt="">
+			</a>
+		</div>
+		<div class="item gts col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-vector/happy-freelancer-with-computer-home-young-man-sitting-armchair-using-laptop-chatting-online-smiling-vector-illustration-distance-work-online-learning-freelance_74855-8401.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-vector/happy-freelancer-with-computer-home-young-man-sitting-armchair-using-laptop-chatting-online-smiling-vector-illustration-distance-work-online-learning-freelance_74855-8401.jpg" alt="">
+			</a>
+		</div>
+		<div class="item selfie col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-psd/front-view-photographer-wooden-workspace-with-computer-monitor_23-2149021348.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-psd/front-view-photographer-wooden-workspace-with-computer-monitor_23-2149021348.jpg" alt="">
+			</a>
+		</div>
+		<div class="item gts col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-photo/laptop-pens-phone-note-with-blank-screen-table_155003-5339.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-photo/laptop-pens-phone-note-with-blank-screen-table_155003-5339.jpg" alt="">
+			</a>
+		</div>
+		<div class="item selfie col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-psd/modern-computer-screen-mockup-v3_317037-127.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-psd/modern-computer-screen-mockup-v3_317037-127.jpg" alt="">
+			</a>
+		</div>
+		<div class="item gts col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-vector/work-from-home-illustration-with-copy-space-man-working-with-laptop-desktop-flat-design_115968-28.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-vector/work-from-home-illustration-with-copy-space-man-working-with-laptop-desktop-flat-design_115968-28.jpg" alt="">
+			</a>
+		</div>
+		<div class="item gts col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-psd/computer-screen-blank-background-mock-up_165789-570.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-psd/computer-screen-blank-background-mock-up_165789-570.jpg" alt="">
+			</a>
+		</div>
+		<div class="item gts col-lg-3 col-md-4 col-6 ">
+			<a href="https://img.freepik.com/free-vector/happy-freelancer-with-computer-home-young-man-sitting-armchair-using-laptop-chatting-online-smiling-vector-illustration-distance-work-online-learning-freelance_74855-8401.jpg" class="fancylight popup-btn" data-fancybox-group="light">
+				<img class="img-fluid" src="https://img.freepik.com/free-vector/happy-freelancer-with-computer-home-young-man-sitting-armchair-using-laptop-chatting-online-smiling-vector-illustration-distance-work-online-learning-freelance_74855-8401.jpg" alt="">
+			</a>
+		</div>
+	</div>
+<div>
+	<p class="text-muted text-end moveTo"><a href="./pages/gallery.html">See more</a></p>
+</div>
+
+
+</div>
+
+
+
+
+<!-- team section start here -->
+<div class="container py-5">
+	<div class="container-info text-center pb-4">
+		<h1>Team mates</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+	</div>
+	<div class="container">
+		
+		
+		<div class="justify-content-center row">
+			<div class="col-6 p-3 col-lg-2 col-md-5 col-sm-6 p-md-3 p-sm-3">
+				<div class="bg-transparent border-0 card m-auto overflow-hidden rounded-circle teamMember">
+					<div class="w-100 h-100">
+						<img class="card-img-top rounded-circle h-100 fitImg" src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="Card image cap" >
+					</div>
+					
+					<div class="card-body position-absolute memberInfo pt-1" >
+						<h3 class="card-title text-white m-0">Jhone</h3>
+						<p class="card-text text-white">ID : 45454</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 p-3 col-lg-2 col-md-5 col-sm-6 p-md-3 p-sm-3">
+				<div class="bg-transparent border-0 card m-auto overflow-hidden rounded-circle teamMember">
+					<div class="w-100 h-100">
+						<img class="card-img-top rounded-circle h-100 fitImg" src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="Card image cap" >
+					</div>
+					
+					<div class="card-body position-absolute memberInfo pt-1" >
+						<h3 class="card-title text-white m-0">Jhone</h3>
+						<p class="card-text text-white">ID : 45454</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 p-3 col-lg-2 col-md-5 col-sm-6 p-md-3 p-sm-3">
+				<div class="bg-transparent border-0 card m-auto overflow-hidden rounded-circle teamMember">
+					<div class="w-100 h-100">
+						<img class="card-img-top rounded-circle h-100 fitImg" src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="Card image cap" >
+					</div>
+					
+					<div class="card-body position-absolute memberInfo pt-1" >
+						<h3 class="card-title text-white m-0">Jhone</h3>
+						<p class="card-text text-white">ID : 45454</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 p-3 col-lg-2 col-md-5 col-sm-6 p-md-3 p-sm-3">
+				<div class="bg-transparent border-0 card m-auto overflow-hidden rounded-circle teamMember">
+					<div class="w-100 h-100">
+						<img class="card-img-top rounded-circle h-100 fitImg" src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="Card image cap" >
+					</div>
+					
+					<div class="card-body position-absolute memberInfo pt-1" >
+						<h3 class="card-title text-white m-0">Jhone</h3>
+						<p class="card-text text-white">ID : 45454</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 p-3 col-lg-2 col-md-5 col-sm-6 p-md-3 p-sm-3">
+				<div class="bg-transparent border-0 card m-auto overflow-hidden rounded-circle teamMember">
+					<div class="w-100 h-100">
+						<img class="card-img-top rounded-circle h-100 fitImg" src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="Card image cap" >
+					</div>
+					
+					<div class="card-body position-absolute memberInfo pt-1" >
+						<h3 class="card-title text-white m-0">Jhone</h3>
+						<p class="card-text text-white">ID : 45454</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-6 p-3 col-lg-2 col-md-5 col-sm-6 p-md-3 p-sm-3">
+				<div class="bg-transparent border-0 card m-auto overflow-hidden rounded-circle teamMember" >
+					<div class="w-100 h-100">
+						<img class="card-img-top rounded-circle h-100 fitImg" src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="Card image cap" >
+					</div>
+					
+					<div class="card-body position-absolute memberInfo pt-1" >
+						<h3 class="card-title text-white m-0">Jhone</h3>
+						<p class="card-text text-white">ID : 45454</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<p class="text-muted text-end moveTo"><a href="./pages/teammates.html">See more</a></p>
+	  </div>
+
+	</div>
+
+</div>
+
+
+<!-- contact start here -->
+
+<div class="contact" id="contact">
+      <div class="container py-5">
+	    <div class="container-info text-center pb-4">
+			<h1>Contact</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+		</div>
+        <div class="row">
+          <div class="col-md-4 ">
+             <div class="row" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-lg-12 col-6 p-3 col-lg-12 m-auto">
+                  <div class="info-box mb-4 cap">
+                 <i class="fa-solid fa-location-dot"></i>
+                    <h3>Our Address</h3>
+                    <p>A108 Adam Street, New York, NY 535022</p><br>
+                    
+                    <i class="fa-solid fa-phone"></i>
+                    <h3>Call Us</h3>
+                    <p>+1 5589 55488 55</p>
+                    <p>+1 5589 55488 67</p>
+                  </div>
+                </div>
+
+              </div>
+          </div>
+          <div class="col-md-8 px-5 py-4 align-items-center d-flex" data-aos="fade-up" data-aos-delay="100">
+            <form method="post" action="">
+              <div class="row">
+                <div class="col">
+                  <div class="form-group">
+                    <input type="text" name="fullname" class="form-control" placeholder="fullname" id="name" required>
+                    <p id="error"></p>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" id="email" required>
+                  </div>
+                </div>
+              </div>
+              
+              <div class="form-group">
+                <input type="text" name="subject" class="form-control" placeholder="Subject" id="subject" required>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" name="msg" style="height: 150px;" id="msg" required></textarea>
+              </div>
+              <div class="form-group">
+                
+                <input type="submit" name="submit" class="btn btn-primary" id="submit" >
+              </div>
+            </form>
+          </div>
+        </div>
+
+      </div>
+
+
+<!-- footer start here -->
+
+<div class="mt-5 pt-5 pb-2 footer">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-5 col-xs-12 about-company pb-4">
+      <h2>DIGITAL</h2>
+      <p class="pr-5 text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante mollis quam tristique convallis </p>
+   
+    </div>
+    <div class="col-lg-3 col-xs-12 links pb-4">
+      <h4 class="mt-lg-0 mt-sm-3 text-white">Useful Links</h4>
+        <ul class="m-0 p-0">
+          <li>- <a href="#">Home</a></li>
+          <li>- <a href="./pages/about.html">About</a></li>
+          <li>- <a href="./pages/gallery.html">Gallery</a></li>
+          <li>- <a href="./pages/teammates.html">Team mates</a></li>
+          <li>- <a href="#contact">Contact</a></li>
+        </ul>
+    </div>
+    <div class="col-lg-4 col-xs-12 location pb-4 ">
+      <h4 class="mt-lg-0 mt-sm-4 text-white">Location</h4>
+      <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
+      <p class="mb-0"><i class="fa fa-phone mr-3"></i>(541) 754-3010</p>
+      <p><i class="fa fa-envelope-o mr-3"></i>info@hsdf.com</p>
+    </div>
+  </div>
+  <div class="row mt-1">
+    <div class="col copyright">
+      <p class="pt-2 text-center"><small class="text-white-50">© 2022 Copyright: A2 Web Developers</small></p>
+    </div>
+  </div>
+</div>
+</div>
+
+
+{{--  JS --}}
+  <script src="{{url('frontend/./assets/js/jquery.min.js')}}"></script>
+  <script src="{{url('frontend/./assets/js/popper.js')}}"></script>
+  <script src="{{url('frontend/./assets/js/bootstrap.min.js')}}"></script>
+  <script src="{{url('frontend/./assets/js/main.js')}}"></script>
+  <script src="{{url('frontend/./assets/js/slider.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js"></script>
+
+  <script src="{{url('frontend/./assets/js/filter.js')}}"></script>
+
+	</body>
+</html>
